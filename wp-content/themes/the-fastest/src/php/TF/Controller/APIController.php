1771dc41f879254416e8dcb383e875265232a2c5
Alejandro Sanchez <aalejo@gmail.com>
2020-11-02 23:14:11 -0500

    Update APIController.php

--- wp-content/themes/the-fastest/src/php/TF/Controller/APIController.php
+++ wp-content/themes/the-fastest/src/php/TF/Controller/APIController.php
@@ -1,225 +1,176 @@
 <?php
 
-namespace TF\Controller;
+namespace WPAS\Controller;
 
-use WPAS\Exception\WPASException;
-use TF\Types\EventPostType;
-use TF\Types\CoursePostType;
-use TF\Types\LocationPostType;
-use \WP_Query;
-use \WPAS\Settings\WPASThemeSettingsBuilder;
+require_once('global_functions.php');
 
-class APIController{
+use WPAS\Utils\WPASException;
+use WPAS\Utils\WPASLogger;
+
+class WPASAPIController{
     
-    public function getAllEvents(){
-        return EventPostType::all();
-    }
+    private $routes = [];
+    private $options = [];
+    private $closures = [];
     
-    public function getAllCourses(){
-        return CoursePostType::getUpcomingDates(true);
-    }
-    
-    public function getAllLocations(){
-        $lang = 'en';
-        if(!empty($_GET['lang'])) $lang = $_GET['lang'];
-        return LocationPostType::all(['lang' => $lang]);
-    }
-    
-    public function getPrices(){
+    public function __construct($options=[]){
         
-        $course = null;
-        if(isset($_GET['course'])) $course = $_GET['course'];
-        $location = null;
-        if(isset($_GET['location'])) $location = $_GET['location'];
+        WPASLogger::getLogger('wpas_controller');
+        
+        $this->options = [
+            'namespace' => '',
+            'application_name' => null,
+            'version' => null,
+            'allow-origin' => '*',
+            'allow-methods' => 'GET'
+            ];
+        $this->loadOptions($options);
+        
+        /**
+         * Only allow GET requests
+         */
+        add_action( 'rest_api_init', function() {
+            
+        	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+        	add_filter( 'rest_pre_serve_request', function( $value ) {
+        	    
+        		header( 'Access-Control-Allow-Origin: '.$this->options['allow-origin']);
+        		header( 'Access-Control-Allow-Methods: '.$this->options['allow-methods']);
+        
+        		return $value;
+        		
+        	});
+        }, 15 );
+        
+        add_action( 'rest_api_init', [$this,'load']);
+    }
+    
+    private function loadOptions($options){
+        foreach($this->options as $key => $val) 
+            if(isset($options[$key])) 
+                $this->options[$key] = $options[$key];
+                
+        if(!isset($options['application_name'])) throw new WPASException('The WPAS_APIController expects an application_name option');
+        if(!isset($options['version'])) throw new WPASException('The WPAS_APIController expects an version option');
+    }
+    
+    private function is_closure($t) {
+        return is_callable($t);
+    }
+    
+    public function get($args){ $this->route($args, 'GET'); }
+    public function post($args){ $this->route($args, 'POST'); }
+    public function put($args){ $this->route($args, 'PUT'); }
+    public function delete($args){ $this->route($args, 'DELETE'); }
+    public function route($args, $method){
+        
+        if(!isset($args['path'])) throw new WPASException('You need to specify the path parameter for the endpoint');
+        if(!isset($args['controller'])) throw new WPASException('You need to specify the controller parameter for the endpoint');
+        $path = $args['path'];
+        
+        $controller = $args['controller'];
+        $closureIndex = $controller;
+        if($this->is_closure($controller)){
 
-        if($course){
-            $prices = include(dirname(__FILE__).'/../../languages/prices.'.$course.'.php');
-            if($prices){
-                if($location) $prices = $prices[$location];
-                return [
-                    "status" => "ok",
-                    "code" => 200,
-                    "data" => $prices
+            $closureIndex = "closure_".spl_object_hash($controller);
+            $this->closures[$closureIndex] = [
+                'action' => $args['controller'],
+                'closure' => $controller
+            ];
+        }
+        $capability = '';
+        if( isset( $args['capability'] ) && $args['capability'] ){
+            $capability = $args['capability'];
+        }
+        
+        $endpointArgs = '';
+        if(isset( $args['args'] ) ){
+            $endpointArgs = $args['args'];
+        }
+        
+        if( !array_key_exists($path, $this->routes) ){
+            
+            $this->routes[$path] = [ 
+                    [
+                        'callback' => $closureIndex , 
+                        'method' => $method, 
+                        'permission_callback' => $capability,
+                        'args' => $endpointArgs
+                    ]
                 ];
+            
+        }else{
+            array_push($this->routes[$path], array('callback' => $closureIndex , 'method' => $method, 'permission_callback' => $capability, 'args' => $endpointArgs));
+        }
+    }
+    
+    public function load(){
+
+        WPASLogger::info('WPAS_APIController: INIT');
+        foreach($this->routes as $path => $listOfMethods){
+            foreach($listOfMethods as $params){
+            
+                $controller = $params['callback'];
+                $httpMethod = $params['method'];
+                $capability = $params['permission_callback'];
+                $args = $params['args'];
+                
+                $controllerObject = $this->getController($controller);
+                $className = $controllerObject;
+                $methodName = '';
+                if(is_array($className))
+                {
+                    $methodName = $controllerObject[1]; //The view
+                    $className = $controllerObject[0]; //The type of the view
+                }else{
+                    if(strpos($controller, 'closure_') === false) throw new WPASException('You need to specify the controller and class method that will handle the API request');
+                } 
+                
+                $initArray = array('methods' => $httpMethod);
+                
+                if(isset($this->closures[$controller])){
+                    
+                    $initArray['callback'] = $this->closures[$controller]['closure'];
+                    
+                }else{
+
+                    WPASLogger::info('WPAS_APIController: match found for '.$httpMethod.': '.$path.', controller => '.$controller.' ] calling: '.$methodName);
+                    $controller = $this->options['namespace'].$className;
+                    
+                    $v = new $controller();
+                    
+                    if(is_callable([$v,$methodName])){
+                        $initArray['callback'] = [$v,$methodName];
+                    }
+                    else throw new WPASException('Method "'.$methodName.'" for api path "'.$path.'" does not exists in '.$className);
+                }
+                
+                if( !empty($capability)){
+                    $initArray['permission_callback'] = function ($request) use ($capability) {
+                        if (current_user_can($capability))
+                        return true;
+                    };
+                }
+                else{
+                    $initArray['permission_callback'] = function ($request) use ($capability) {
+                        return true;
+                    };
+                }
+                
+                if( !empty($args) ){
+                    $initArray['args'] = $args;
+                }
+                
+                register_rest_route( $this->options['application_name'].'/v'.$this->options['version'], $path, $initArray);
             }
         }
-        else return [
-            "status" => "ok",
-            "code" => 200,
-            "data" => include(dirname(__FILE__).'/../../languages/all.prices.php')
-        ];
-        
-        return [
-            "status" => "error",
-            "code" => 400,
-            "data" => 'Price not found'
-        ];
     }
     
-    public function syncEvents(){
-        $upcoming = EventPostType::getEventsFromAPI();
-        WPASThemeSettingsBuilder::setThemeOption('sync-bc-events-api',$upcoming);
-        return [
-            "status" => "ok",
-            "code" => 200,
-            "data" => $upcoming
-        ];
-    }
-    
-    public function syncCohorts(){
-        $upcoming = CoursePostType::getCohortsFromAPI();
-        WPASThemeSettingsBuilder::setThemeOption('sync-bc-cohorts-api',$upcoming);
-        return [
-            "status" => "ok",
-            "code" => 200,
-            "data" => $upcoming
-        ];
-    }
-    
-    public function syncProfiles(){
-        $upcoming = CoursePostType::getProfilesFromAPI();
-        WPASThemeSettingsBuilder::setThemeOption('sync-bc-profiles-api',$upcoming);
-        return [
-            "status" => "ok",
-            "code" => 200,
-            "data" => $upcoming
-        ];
-    }
-    
-    public function requestMoreInfo(){
-
-        $listId = get_option('activecampaign-soft-leads-list-id');
-        $utmUrlId = get_option('activecampaign-utm-url-field');
-        $utmLanguageId = get_option('activecampaign-utm-language-field');
-        $utmCountryId = get_option('activecampaign-utm-country-field');
-        $gclid = get_option('activecampaign-gclid-field');
-        $utmLocationId = get_option('activecampaign-utm-location-field');
-        $referralId = get_option('activecampaign-referral-key-field');
-
-        if(empty($_POST['email'])) WPASController::ajaxError('Invalid Email');
-        
-        $fistName = '';
-        if(!empty($_POST['first_name'])) $fistName = $_POST['first_name'];
-        
-        //get WPAS_APP values
-        $globalContext = wpas_get_global_context();
-
-        $utmURLValue = 'undefined';
-        if(isset($globalContext['url'])) $utmURLValue = $globalContext['url'];
-        
-        $utmLanguageValue = 'undefined';
-        if(isset($globalContext['lang'])) $utmLanguageValue = $globalContext['lang'];
-        
-        $utmCountryValue = 'undefined';
-        if(isset($globalContext['country_name'])) $utmCountryValue = $globalContext['country_name'];
-        
-        $gclidValue = 'undefined';
-        if(isset($_GET['gclid'])) $gclidValue = $_GET['gclid'];
-        else if(isset($globalContext['gclid'])) $gclidValue = $globalContext['gclid'];
-
-        $utmLocationValue = 'undefined';
-        if(isset($_GET['utm_location'])) $utmLocationValue = $_GET['utm_location'];
-        else if(isset($globalContext['city_slug'])) $utmLocationValue = $globalContext['city_slug'];
-        
-        $referralValue = 'undefined';
-        if(isset($_GET['referral_key'])) $referralValue = $_GET['referral_key'];
-        else if(isset($globalContext['referral_key'])) $referralValue = $globalContext['referral_key'];
-
-        $contact = array(
-    		"email" => $_POST['email'],
-    		"first_name" => $fistName,
-    		"tags" => 'download_syllabus, request_more_info',
-    		"field[".$utmUrlId.",0]" => $utmURLValue,
-    		"field[".$utmLanguageId.",0]" => $utmLanguageValue,
-    		"field[".$utmCountryId.",0]" => $utmCountryValue,
-    		"field[".$utmLocationId.",0]" => $utmLocationValue,
-    		"field[".$gclid.",0]" => $gclidValue,
-    	);
-    	if(!empty($referralId)) $contact["field[".$referralId.",0]"] = $referralValue;
-
-        $coursesField = \TF\ActiveCampaign\ACAPI::subscribeToList($contact, $listId);
-
-        if($coursesField){
-            return [
-                "status" => "ok",
-                "code" => 200,
-                "data" => "Check your email! In the next minutes your should receive what you asked for :)"
-            ];
-        }
-        else return [
-            "status" => "error",
-            "code" => 500,
-            "data" => "Something went wrong"
-        ];
-        
-    }
-    
-    public function requestTour(){
-
-        $listId = get_option('activecampaign-soft-leads-list-id');
-        $utmUrlId = get_option('activecampaign-utm-url-field');
-        $utmLanguageId = get_option('activecampaign-utm-language-field');
-        $utmCountryId = get_option('activecampaign-utm-country-field');
-        $gclid = get_option('activecampaign-gclid-field');
-        $utmLocationId = get_option('activecampaign-utm-location-field');
-        $referralId = get_option('activecampaign-referral-key-field');
-
-        if(empty($_POST['email'])) WPASController::ajaxError('Invalid Email');
-        
-        $fistName = '';
-        if(!empty($_POST['first_name'])) $fistName = $_POST['first_name'];
-        
-        //get WPAS_APP values
-        $globalContext = wpas_get_global_context();
-
-        $utmURLValue = 'undefined';
-        if(isset($globalContext['url'])) $utmURLValue = $globalContext['url'];
-        
-        $utmLanguageValue = 'undefined';
-        if(isset($globalContext['lang'])) $utmLanguageValue = $globalContext['lang'];
-        
-        $utmCountryValue = 'undefined';
-        if(isset($globalContext['country_name'])) $utmCountryValue = $globalContext['country_name'];
-        
-        $gclidValue = 'undefined';
-        if(isset($_GET['gclid'])) $gclidValue = $_GET['gclid'];
-        else if(isset($globalContext['gclid'])) $gclidValue = $globalContext['gclid'];
-
-        $utmLocationValue = 'undefined';
-        if(isset($_GET['utm_location'])) $utmLocationValue = $_GET['utm_location'];
-        else if(isset($globalContext['city_slug'])) $utmLocationValue = $globalContext['city_slug'];
-        
-        $referralValue = 'undefined';
-        if(isset($_GET['referral_key'])) $referralValue = $_GET['referral_key'];
-        else if(isset($globalContext['referral_key'])) $referralValue = $globalContext['referral_key'];
-
-        $contact = array(
-    		"email" => $_POST['email'],
-    		"first_name" => $fistName,
-    		"tags" => 'request_tour',
-    		"field[".$utmUrlId.",0]" => $utmURLValue,
-    		"field[".$utmLanguageId.",0]" => $utmLanguageValue,
-    		"field[".$utmCountryId.",0]" => $utmCountryValue,
-    		"field[".$utmLocationId.",0]" => $utmLocationValue,
-    		"field[".$gclid.",0]" => $gclidValue,
-    	);
-    	if(!empty($referralId)) $contact["field[".$referralId.",0]"] = $referralValue;
-
-        $coursesField = \TF\ActiveCampaign\ACAPI::subscribeToList($contact, $listId);
-
-        if($coursesField){
-            return [
-                "status" => "ok",
-                "code" => 200,
-                "data" => "Check your email! In the next minutes with possible schedules to organize a tour to our location"
-            ];
-        }
-        else return [
-            "status" => "error",
-            "code" => 500,
-            "data" => "Something went wrong"
-        ];
-        
+    private function getController($controller){
+        $pieces = explode(':',$controller);
+        if(count($pieces)==1) return $pieces[0];
+        else if(count($pieces)==2) return [$pieces[0],$pieces[1]];
+        else throw new WPASException('The controller '.$controller.' is invalid');
     }
     
 }
